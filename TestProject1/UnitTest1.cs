11bd38cc07cf8974ab7517a6e3fde692ef4fe022
mlodydev <modyhipi@gmail.com>
2022-01-04 19:52:09 +0100

    test: add test for method getscore, submit other changes in test project

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClinicApi;
+
 namespace TestProject1
 {
     [TestClass]
@@ -16,6 +18,26 @@ namespace TestProject1
 
             //Assert
             Assert.AreEqual(2, two);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            int x1 = 4;
+            int x2 = 0;
+            int x3 = 0;
+            int x4 = 0;
+            int x5 = 0;
+            int x6 = 0;
+            float x7 = 0;
+            float x8 = 0;
+
+            //Act
+            float Score_test = ClinicApi.Controllers.UserDiagnoseController.GetScore(x1, x2, x3, x4, x5, x6, x7, x8);
+
+            //Assert
+            Assert.AreEqual(4, Score_test);
         }
     }
 }
